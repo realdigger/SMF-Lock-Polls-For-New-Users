cb03fe084d5fabf9d6de48968a26c337f6c18dcf
Oleg <oleg.ostrverhy@gmail.com>
2012-09-15 07:46:27 +0400

    initial release

---
+++ Mod-BlockPollsForNewUsers.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package SMF Block Polls For New Users
+ * @author digger
+ * @copyright 2012
+ * @license CC BY-NC-ND http://creativecommons.org/licenses/by-nc-nd/3.0/
+ * @version 1.0
+ */
+
+if (!defined('SMF'))
+    die('Hacking attempt...');
+ 
+
+ 
